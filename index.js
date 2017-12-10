cd1f09f9b9f6ec586f068d9ff63267f3d529767b
Harry Shipton <harryjshipton@gmail.com>
2017-12-10 18:06:12 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 (function() {
+  let option = false;
+  $('#optSel').change('change', function(e) {
+    let selOpt = $('option:selected', this).value;
+    console.log(selOpt);
+  });
+  $('#inTA').bind('input propertychange', function() {
+    console.log('test');
+  });
 })();
